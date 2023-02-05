542b523a8291a2d58a72e74a021ee1ba88c4345d
SevaStopAll <Sevastopallrh@yandex.ru>
2023-02-05 20:12:06 +0400

    2. Парсинг HTML страницы. [#260358] My first successful parcing I am very happy about it

---
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -0,0 +1,31 @@
+package ru.job4j.grabber;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class HabrCareerParse {
+
+    private static final String SOURCE_LINK = "https://career.habr.com";
+
+    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
+
+    public static void main(String[] args) throws IOException {
+        Connection connection = Jsoup.connect(PAGE_LINK);
+        Document document = connection.get();
+        Elements rows = document.select(".vacancy-card__inner");
+        rows.forEach(row -> {
+            Element titleElement = row.select(".vacancy-card__title").first();
+            Element dateElement = row.select(".vacancy-card__date").first().child(0);
+            Element linkElement = titleElement.child(0);
+            String vacancyName = titleElement.text();
+            String data = dateElement.attr("datetime").substring(0,10);
+            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+            System.out.printf("%s %s %s%n", vacancyName, data, link);
+        });
+    }
+}
