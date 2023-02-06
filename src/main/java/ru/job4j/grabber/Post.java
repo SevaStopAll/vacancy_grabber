869de00834940af0ca4e6f2c9f0a94a8edb33ee8
SevaStopAll <Sevastopallrh@yandex.ru>
2023-02-06 20:58:48 +0400

    2.2. Модель данных - Post. [#285211 #388767] Remake equals and hashcode methods, created new constructors.

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -10,9 +10,25 @@ public class Post {
     private String description;
     private LocalDateTime created;
 
+    public Post(int id, String name, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.name = name;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public Post(String name, String link, String description, LocalDateTime created) {
+        this.name = name;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
     public int getId() {
         return id;
     }
+
 
     public void setId(int id) {
         this.id = id;
@@ -40,19 +56,15 @@ public class Post {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         Post post = (Post) o;
-        return Objects.equals(link, post.link) && Objects.equals(description, post.description) && Objects.equals(created, post.created);
+        return id == post.id && Objects.equals(link, post.link);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(link, description, created);
+        return Objects.hash(id, link);
     }
 
     @Override
