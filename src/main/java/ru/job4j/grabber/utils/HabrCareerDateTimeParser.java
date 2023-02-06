b66768c55b6b2e215d227c7280a78944916d3c24
SevaStopAll <Sevastopallrh@yandex.ru>
2023-02-06 21:12:22 +0400

    2.1. Преобразование даты [#289476 #388763] Add when to my test. Add CONSTANT to my formatter and changed it type (with TimeZone)

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -1,13 +1,16 @@
 package ru.job4j.grabber.utils;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+import static java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
+    private static final DateTimeFormatter FORMATTER = ISO_OFFSET_DATE_TIME;
 
     @Override
     public LocalDateTime parse(String parse) {
-        return LocalDateTime.parse(parse, ISO_LOCAL_DATE_TIME);
+        return LocalDateTime.parse(parse, FORMATTER);
     }
 }
--- src/test/java/ru/job4j/grabber/HabrCareerParseTest.java
+++ src/test/java/ru/job4j/grabber/HabrCareerParseTest.java
@@ -5,7 +5,7 @@ import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.time.LocalDateTime;
 
-import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+import static java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 import static org.assertj.core.api.Assertions.*;
 
 class HabrCareerParseTest {
@@ -13,8 +13,8 @@ class HabrCareerParseTest {
     @Test
     void parseRightDate() {
         HabrCareerDateTimeParser parcer = new HabrCareerDateTimeParser();
-        String date = "2023-02-05T13:35:09";
-        LocalDateTime expected = LocalDateTime.parse("2023-02-05T13:35:09", ISO_LOCAL_DATE_TIME);
+        String date = "2023-02-06T15:27:50+03:00";
+        LocalDateTime expected = LocalDateTime.parse("2023-02-06T15:27:50+03:00", ISO_OFFSET_DATE_TIME);
         LocalDateTime result = parcer.parse(date);
         assertThat(result).isEqualTo(expected);
     }
