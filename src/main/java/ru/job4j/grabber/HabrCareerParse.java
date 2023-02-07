3a13db637a71d855a94a9d17f1b75bbb3380dfe1
Sevastopall <Sevastopallrh@yandex.ru>
2023-02-07 08:36:01 +0400

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1 [#285210] Try to parse more than 1 page

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -17,17 +17,17 @@ public class HabrCareerParse {
     public static void main(String[] args) throws IOException {
         for (int index = 1; index <= 5; index++) {
             page = index;
-        Connection connection = Jsoup.connect(PAGE_LINK);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
-        rows.forEach(row -> {
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element dateElement = row.select(".vacancy-card__date").first().child(0);
-            Element linkElement = titleElement.child(0);
-            String vacancyName = titleElement.text();
-            String data = dateElement.attr("datetime");
-            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            System.out.printf("%s %s %s%n", vacancyName, data, link);
+            Connection connection = Jsoup.connect(PAGE_LINK);
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element dateElement = row.select(".vacancy-card__date").first().child(0);
+                Element linkElement = titleElement.child(0);
+                String vacancyName = titleElement.text();
+                String data = dateElement.attr("datetime");
+                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                System.out.printf("%s %s %s%n", vacancyName, data, link);
         });
         }
     }
