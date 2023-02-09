ad7e53eaa23dd292239c7a635f94fc6a74618bb7
Sevastopall <Sevastopallrh@yandex.ru>
2023-02-09 08:19:11 +0400

    6. Grabber [#289477] Pre-commit class Grabber, united all made before.

---
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -0,0 +1,68 @@
+package ru.job4j.grabber;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
+
+import java.io.*;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+public class Grabber implements Grab {
+    private final Parse parse;
+    private final Store store;
+    private final Scheduler scheduler;
+    private final int time;
+
+    public Grabber(Parse parse, Store store, Scheduler scheduler, int time) {
+        this.parse = parse;
+        this.store = store;
+        this.scheduler = scheduler;
+        this.time = time;
+    }
+
+    @Override
+    public void start() throws SchedulerException {
+        JobDataMap data = new JobDataMap();
+        data.put("store", store);
+        data.put("parse", parse);
+        JobDetail job = newJob(GrabJob.class)
+                .usingJobData(data)
+                .build();
+        SimpleScheduleBuilder times = simpleSchedule()
+                .withIntervalInSeconds(time)
+                .repeatForever();
+        Trigger trigger = newTrigger()
+                .startNow()
+                .withSchedule(times)
+                .build();
+        scheduler.scheduleJob(job, trigger);
+    }
+
+    public static class GrabJob implements Job {
+        @Override
+        public void execute(JobExecutionContext context) {
+            JobDataMap map = context.getJobDetail().getJobDataMap();
+            Store store = (Store) map.get("store");
+            Parse parse = (Parse) map.get("parse");
+            /* TODO impl logic */
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        var cfg = new Properties();
+        try (InputStream in = Grabber.class.getClassLoader()
+                .getResourceAsStream("app.properties")) {
+            cfg.load(in);
+        }
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        scheduler.start();
+        var parse = new HabrCareerParse(new HabrCareerDateTimeParser());
+        var store = new PsqlStore(cfg);
+        var time = Integer.parseInt(cfg.getProperty("time"));
+        new Grabber(parse, store, scheduler, time).start();
+    }
+}
