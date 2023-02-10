f954f6fbde1683108c586ee22dc4508718d6e1ec
SevaStopAll <Sevastopallrh@yandex.ru>
2023-02-10 21:07:03 +0400

    7. Web. [#289478] Created my Pre-commit with prototype of web() method.

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -5,6 +5,8 @@ import org.quartz.impl.StdSchedulerFactory;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.newJob;
@@ -52,6 +54,34 @@ public class Grabber implements Grab {
         }
     }
 
+    public void web(Store store) {
+        var cfg = new Properties();
+        try (InputStream in = Grabber.class.getClassLoader()
+                .getResourceAsStream("app.properties")) {
+            cfg.load(in);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes());  //post.toString().getBytes(Charset.forName("Windows-1251"));
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
     public static void main(String[] args) throws Exception {
         var cfg = new Properties();
         try (InputStream in = Grabber.class.getClassLoader()
@@ -65,4 +95,13 @@ public class Grabber implements Grab {
         var time = Integer.parseInt(cfg.getProperty("time"));
         new Grabber(parse, store, scheduler, time).start();
     }
+
+   /* public static void main(String[] args) throws Exception {
+        Grabber grab = new Grabber();
+        grab.cfg();
+        Scheduler scheduler = grab.scheduler();
+        Store store = grab.store();
+        grab.init(new HabrCareerParse(new HabrCareerDateTimeParser()), store, scheduler);
+        grab.web(store);
+    }*/
 }
